0d3d8e8688736a0c89529b3aa2070a4f30d9ea55
Deni Cabaravdic <37716587+d3h1@users.noreply.github.com>
2021-03-03 20:35:05 -0800

    Update ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -5,35 +5,61 @@
 /************************************/
 
 
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-
-#define max_int (255)
-#define byte unsigned char
+#include <math.h>
 
 int main (int argc, char * argv[], char ** envp)
 {
-    int offset = 0x30; // 0x38 = 48
-    byte ascii_value;
-    int number = 0;
-    int digit;
-    int retval
+        const char MAX_POWER = 32; 
         
-    if(retval != 0 || retval != 1)
-    {
-        fprintf(stderr, "Mistake found, look at it again\n"); 
-        return 1;
+        char count = 0; 
+        char ascii_value = 0;
+        char offset = 0x30;                 
+        char digit = 0;
+        unsigned int number = 0;  
+        int retval = 0;
+        
+        retval = read(0, &ascii_value, 1); 
+        
+            while (retval == 1)
+            {
+                                                                    
+                if(ascii_value == 0x30 || ascii_value == 0x31)      
+                {                                                  
+                    count++;                   
+                    digit = ascii_value - offset;                  
+                    number = (number << 1) + digit;                
+                    retval = read(0, &ascii_value, 1);  
+                }
+                else                                               
+                {                                                
+                        retval = 0;                                 
+                }                                                  
+                                                                    
+            }
+            
+    if((ascii_value == 0x30 || ascii_value == 0x31 || ascii_value == 0x0a) && MAX_POWER >= count) 
+    {   
+        printf("%u\n", number);                                 
+        return 0;                                               
+    }                                                           
+    else                                        
+    {                                           
+        if(MAX_POWER < count)                                            
+        {                                                               
+            printf("Sorry, the number calculated exceeds 2^32. " 
+            "Please enter a binary number that doesn't exceed 2^32. \n");
+        }
+        else                                                            
+        {                                                               
+            printf("A number that is not a 1 or 0 has been entered. " 
+            "Please enter a number in binary with only 1s and 0s. \n");
+        }
+        return 1;                                           
     }
-    
-    retval = read(0, &ascii_value, 1);
-    while (retval == 1)
-        digit = ascii_value - offset;
-        number = (number << 1) + digit;  
-        retval = read(0, &ascii_value, 1);
-        
+}
     printf("%u\n", number);
     return 0;
-
 }
