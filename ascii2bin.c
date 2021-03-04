519afcba8aa1eb9b89ff40b03119aeecee576d43
Deni Cabaravdic <37716587+d3h1@users.noreply.github.com>
2021-03-03 20:18:12 -0800

    Update ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -5,29 +5,30 @@
 /************************************/
 
 
-#include <stdio.h>
+#include "stdio.h"
+#include "stdlib.h"
 #include <unistd.h>
 
-int main()
+int main(int argc, char * argv[], char ** envp)
 {
 
 	char ascii_value;
-	int offset = 48; 
+	char digit;
+	int count = 0;
+	int offset = 48;
 	int number = 0;
+	int retval;
+
+retval = read(0, &ascii_value, 1);
+{
+	while (retval == 1)
+		count++;
+}
 	
+digit = ascii_value - offset;
+number = (number << 1) + digit;
+retval = read(0, &ascii_value, 1);
 
-int retval = read(0, &ascii_value, 1);
-
-while ((retval == 1) && (ascii_value != '\n')) 
-    {
-        int digit = ascii_value - offset;
-
-        if ((digit == 1) || (digit == 0))
-            number = (number << 1) + digit;  
-                
-        retval = read(0, &ascii_value, 1);
-    }
-
-printf("%d\n", number);
+printf("%u\n", number);
 return 0;
 }
